05579ef7cadcf98b40e841e4c33673b2603cd687
Kunal Patil <kunallanuk9@gmail.com>
2025-06-23 10:10:07 +0530

    Update useHtmlUrl.ts

--- src/hooks/useHtmlUrl.ts
+++ src/hooks/useHtmlUrl.ts
@@ -1,5 +1,16 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Generates a temporary Blob URL from HTML content.
+ * Can be used to:
+ * - Open the HTML in a new tab using `window.open(blobUrl)` or in an `href` attritbute.
+ * - Embed it in an <iframe> or any custom render logic
+ * - Avoid external hosting or CORS issues
+ *
+ * @param {string} htmlContent - The full HTML content as a string
+ * @returns {string} - A temporary Blob URL
+ */
+
 export const useHtmlUrl = (htmlContent: string): string | null => {
   const [iframeSrc, setIframeSrc] = useState<string | null>(null);
 
